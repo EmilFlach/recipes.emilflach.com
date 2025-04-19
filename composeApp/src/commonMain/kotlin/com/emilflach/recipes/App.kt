ef462bf75547bb19248645c209ad2641fd6f8b92
Emil Flach <emil.flach@jetbrains.com>
2025-04-19 17:44:44 +0200

    First content for Recipe details screen

--- composeApp/src/commonMain/kotlin/com/emilflach/recipes/App.kt
+++ composeApp/src/commonMain/kotlin/com/emilflach/recipes/App.kt
@@ -1,5 +1,9 @@
 package com.emilflach.recipes
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -7,9 +11,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.emilflach.recipes.data.RecipeRepository
-import com.emilflach.recipes.ui.screens.RecipesScreen
 import com.emilflach.recipes.ui.screens.RecipeDetailScreen
 import com.emilflach.recipes.ui.screens.RecipeDetailViewModel
+import com.emilflach.recipes.ui.screens.RecipesScreen
 import com.emilflach.recipes.ui.screens.RecipesViewModel
 import com.emilflach.recipes.ui.screens.Screen
 
@@ -20,7 +24,20 @@ fun App() {
     val recipeDetailViewModel = RecipeDetailViewModel(RecipeRepository())
 
     NavHost(navController = navController, startDestination = Screen.RecipesList.route) {
-        composable(Screen.RecipesList.route) {
+        composable(
+            Screen.RecipesList.route,
+            enterTransition = {
+                fadeIn(
+                    animationSpec = tween(50)
+                )
+            },
+            exitTransition = {
+
+                fadeOut(
+                    animationSpec = tween(1000)
+                )
+            }
+        ) {
             RecipesScreen(
                 viewModel = recipesViewModel,
                 onRecipeClick = { recipeId ->
@@ -31,7 +48,19 @@ fun App() {
 
         composable(
             route = Screen.RecipeDetail.route,
-            arguments = listOf(navArgument("recipeSlug") { type = NavType.StringType })
+            arguments = listOf(navArgument("recipeSlug") { type = NavType.StringType }),
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(300)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(300)
+                )
+            },
         ) { backStackEntry ->
             val recipeSlug = backStackEntry.arguments?.getString("recipeSlug")
             recipeSlug?.let {
--- composeApp/src/commonMain/kotlin/com/emilflach/recipes/ui/screens/RecipeDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/emilflach/recipes/ui/screens/RecipeDetailScreen.kt
@@ -1,20 +1,20 @@
 package com.emilflach.recipes.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -23,6 +23,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.emilflach.recipes.RecipesAppTheme
@@ -43,49 +47,112 @@ fun RecipeDetailScreen(
         viewModel.loadRecipeBySlug(recipeSlug)
     }
     RecipesAppTheme {
-        Column {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = { Text(text = recipe?.name ?: "") },
-                        navigationIcon = {
-                            IconButton(onClick = onBackClick) {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                                    contentDescription = "Back"
+        LazyColumn(
+            modifier = Modifier.background(MaterialTheme.colors.background)
+        ) {
+            item {
+                BoxWithConstraints(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                ) {
+                    if (recipe != null) {
+                        AsyncImage(
+                            model = recipe?.imageUrl,
+                            contentDescription = recipe?.name,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxWidth().fillMaxHeight()
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .background(
+                                Brush.verticalGradient(
+                                    0f to Color.Black,
+                                    0.3f to Color.Transparent,
                                 )
-                            }
-                        }
+                            )
                     )
+
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White,
+                        )
+                    }
                 }
-            ) { paddingValues ->
-                when {
-                    isLoading -> {
+
+            }
+
+            when {
+                isLoading -> {
+                    item {
                         BoxWithConstraints(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
                             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                         }
                     }
 
-                    isError -> {
+                }
+
+                isError -> {
+                    item {
                         Text(
                             text = "Error: $errorMessage",
                             color = MaterialTheme.colors.error,
                             modifier = Modifier.padding(16.dp)
                         )
+
                     }
-                    else -> {
-                        // Add your recipe detail content here
-                        Box(modifier = Modifier.padding(paddingValues)) {
+
+                }
+
+                else -> {
+                    item {
+                        Text(
+                            text = recipe?.name ?: "",
+                            style = MaterialTheme.typography.h1,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth().padding(16.dp),
+                        )
+                    }
+                    item {
+                        Text(
+                            text = "Ingredients",
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 16.dp),
+                        )
+                    }
+                    recipe?.let {
+                        itemsIndexed(it.recipeIngredient) { _, ingredient ->
                             Text(
-                                text = "Recipe detail screen for ${recipe?.name}"
-                            )
-                            AsyncImage(
-                                model = recipe?.imageUrl,
-                                contentDescription = recipe?.name,
-                                modifier = Modifier.height(200.dp).width(200.dp)
+                                text = "${ingredient.quantity} ${ingredient.unit?.name} ${ingredient.food?.name}",
+                                style = MaterialTheme.typography.body1,
+                                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
                             )
                         }
                     }
+                    item {
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = "Procedure",
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                        )
+                    }
+                    recipe?.let {
+                        itemsIndexed(it.recipeInstructions) { index, instruction ->
+                            Text(
+                                text = "${index + 1}. ${instruction.text}",
+                                style = MaterialTheme.typography.body1,
+                                modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+                            )
+                        }
+                    }
+
                 }
             }
         }
